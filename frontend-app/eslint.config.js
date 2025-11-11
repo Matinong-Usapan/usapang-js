f22fb9f244e9b025dc7f7a580d8eb7670d23cd65
Gengee <gengeebreadman@gmail.com>
2025-11-12 00:38:14 +0800

    PATCH: Updated the initial project setup

--- frontend-app/eslint.config.js
+++ frontend-app/eslint.config.js
@@ -1,23 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import baseConfig from '../eslint.config.js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  ...baseConfig,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    files: ['**/*.{ts,tsx,jsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs['recommended-latest'].rules,
+      ...reactRefresh.configs.vite.rules,
     },
   },
-])
+]
