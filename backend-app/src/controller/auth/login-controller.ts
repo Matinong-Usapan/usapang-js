40d5fad44c0e1f0082b59a83cbdaa6133baf2e90
gene09876543 <gene09876543@gmail.com>
2025-11-15 00:30:57 +0800

    UJS-6: applied recommendations

--- backend-app/src/controller/auth/login-controller.ts
+++ backend-app/src/controller/auth/login-controller.ts
@@ -1,19 +1,26 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import LoginService from '../../service/auth/login-service.ts';
+import LoginInterface from '../../interface/auth/iLogin.ts';
 
 const LoginController = {
     userLogin: async (req: Request, res: Response) => {
-        const { email, password }: { email: string; password: string } = req.body;
 
-        if (email.length === 0 || password.length === 0) {
+        const reqBody: LoginInterface = {
+            email: req.body?.email || '',
+            password: req.body?.password || '', 
+        };
+
+        const { email, password } = reqBody;
+        
+        if (!email.length || !password.length) 
             return res.status(400).json({ message: 'Email and password are required.' });
-        }   
+        
         try{
-            await LoginService.login(req.body);
+            await LoginService.login(reqBody);
         } catch(error){
             console.log("LoginController error:", error);
-                return res.status(500).send({
+            return res.status(500).send({
                 message:"internal server error ata, boss",
             }
         );
