6c7d1895e7467ecf5f29e3ad9223fa35e3528288
gene09876543 <gene09876543@gmail.com>
2025-11-15 00:30:57 +0800

    USJ-7: Applied recommendations

--- backend-app/src/service/auth/login-service.ts
+++ backend-app/src/service/auth/login-service.ts
@@ -2,10 +2,13 @@ import type LoginInterface from "../../interface/auth/iLogin.ts";
 import { LoginRepository } from "../../repository/auth/login-repository.ts";
 
 const LoginService = {
-    login: async (params: LoginInterface)=>{
+    login: async (params: LoginInterface) => {
         const {email, password} = params;
+
         console.log(`email: ${email} password ${password} service layer`);
+
         if (email.length === 0 || password.length === 0) throw new Error('Email and password are required.');
+        
         try{
             await LoginRepository.login(email, password);
         } catch(error){
